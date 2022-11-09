a36e1534989b1ac0f8d457604fc42e3d2d5b3b95
Sergio Rodiguez <sergio.elena@live.u-tad.com>
2022-11-09 23:06:27 +0100

    Mi primer Commit, Root Commit

---
+++ src/actividad/SR.java
@@ -0,0 +1,9 @@
+package actividad;
+
+public class SR {
+	public static void main (String[] args) {
+		System.out.println("Alumno: Sergio Roriguez");
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Sergio
+ *
+ */
+module Git_Sergio_Rodriguez {
+}
