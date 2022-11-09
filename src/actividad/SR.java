c51279c19c83b8ce705b7f6212cc3e18499b713b
Sergio Rodiguez <sergio.elena@live.u-tad.com>
2022-11-09 23:08:13 +0100

    Modificación 1

--- src/actividad/SR.java
+++ src/actividad/SR.java
@@ -3,7 +3,7 @@ package actividad;
 public class SR {
 	public static void main (String[] args) {
 		System.out.println("Alumno: Sergio Roriguez");
-		
+		System.out.println("Primera Modificación desde Master");
 	}
 
 }
